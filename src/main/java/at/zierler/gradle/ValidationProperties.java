8ff6c68dc65deca1a9f0c3e4cb93dd519cb25af6
Florian Zierler <zierler25@gmail.com>
2017-12-18 16:48:38 +0100

    Set allow duplicates to false

--- src/main/java/at/zierler/gradle/ValidationProperties.java
+++ src/main/java/at/zierler/gradle/ValidationProperties.java
@@ -8,6 +8,6 @@ public class ValidationProperties {
     static final String DEFAULT_DIRECTORY = "src/main/resources/";
 
     private String directory = DEFAULT_DIRECTORY;
-    private boolean allowDuplicates = true;
+    private boolean allowDuplicates = false;
 
 }
