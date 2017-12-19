3fcace812c84a9b26fc84fe39296982476f754cc
Florian Zierler <zierler25@gmail.com>
2017-12-19 09:30:57 +0100

    Rewrite tests

--- src/test/java/at/zierler/gradle/YamlValidatorPluginIntTest.java
+++ src/test/java/at/zierler/gradle/YamlValidatorPluginIntTest.java
@@ -1,9 +1,6 @@
 package at.zierler.gradle;
 
-import org.gradle.testkit.runner.BuildResult;
-import org.gradle.testkit.runner.BuildTask;
 import org.gradle.testkit.runner.GradleRunner;
-import org.gradle.testkit.runner.TaskOutcome;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,7 +12,6 @@ import java.io.IOException;
 import static at.zierler.gradle.YamlValidatorPlugin.TASK_NAME;
 import static org.gradle.util.GFileUtils.writeFile;
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class YamlValidatorPluginIntTest {
@@ -24,115 +20,132 @@ public class YamlValidatorPluginIntTest {
     public final TemporaryFolder testProjectDir = new TemporaryFolder();
 
     private File buildFile;
-    private String yamlDirectory = "src/test/resources/";
+    private String yamlDirectory = ValidationProperties.DEFAULT_DIRECTORY;
     private File yamlFile;
 
     @Before
     public void setupTestProject() throws IOException {
 
-        buildFile = testProjectDir.newFile("build.gradle");
+        this.buildFile = testProjectDir.newFile("build.gradle");
         testProjectDir.newFolder(yamlDirectory.split("/"));
-        yamlFile = testProjectDir.newFile(yamlDirectory + "file.yaml");
-        writeFile("plugins { id 'at.zierler.yamlvalidator' }\n" +
-                "yamlValidator { directory = '" + yamlDirectory + "' }", buildFile);
+        this.yamlFile = testProjectDir.newFile(yamlDirectory + "file.yaml");
     }
 
     @Test
-    public void shouldSetYamlValidatorFileCorrectly() {
+    public void shouldUseDefaultSearchPathsWhenNotOverridden() {
 
-        BuildResult buildResult = GradleRunner
-                .create()
-                .withProjectDir(testProjectDir.getRoot())
-                .withPluginClasspath()
-                .withArguments(TASK_NAME)
-                .build();
+        writeDefaultBuildFileWithoutProperties();
 
-        String output = buildResult.getOutput();
-        BuildTask task = buildResult.task(":" + TASK_NAME);
+        String expectedLineInOutput = "Starting to validate yaml files in " + yamlDirectory + ".";
 
-        assertThat(output, containsString("Starting to validate yaml files in " + yamlDirectory + "."));
-        assertThat(task.getOutcome(), is(TaskOutcome.SUCCESS));
+        expectBuildSuccessAndOutput(expectedLineInOutput);
+    }
+
+    @Test
+    public void shouldUseDefinedSearchPathWhenOverridden() throws IOException {
+
+        String overriddenYamlDirectory = "src/test/resources/";
+        testProjectDir.newFolder(overriddenYamlDirectory.split("/"));
+        writeFile("plugins { id 'at.zierler.yamlvalidator' }\n" +
+                "yamlValidator { directory = '" + overriddenYamlDirectory + "' }", buildFile);
+
+        String expectedLineInOutput = "Starting to validate yaml files in " + overriddenYamlDirectory + ".";
+
+        expectBuildSuccessAndOutput(expectedLineInOutput);
     }
 
     @Test
     public void shouldAllowEmptyYaml() {
 
-        BuildResult buildResult = GradleRunner
-                .create()
-                .withProjectDir(testProjectDir.getRoot())
-                .withPluginClasspath()
-                .withArguments(TASK_NAME)
-                .build();
+        writeDefaultBuildFileWithoutProperties();
 
-        String output = buildResult.getOutput();
-        BuildTask task = buildResult.task(":" + TASK_NAME);
+        String expectedLineInOutput = yamlFile.getAbsolutePath() + " is valid.";
 
-        assertThat(output, containsString(yamlFile.getAbsolutePath() + " is valid."));
-        assertThat(task.getOutcome(), is(TaskOutcome.SUCCESS));
+        expectBuildSuccessAndOutput(expectedLineInOutput);
     }
 
-    @Test
-    public void shouldNotAllowYamlWithDuplicateKey() {
 
-        writeFile("framework:\n  key: value\n\nframework:\n  other: value", yamlFile);
+    @Test
+    public void shouldNotAllowYamlWithDuplicateKeyWhenDuplicationIsEnabled() {
+
+        writeDuplicateKeyYaml();
         writeFile("plugins { id 'at.zierler.yamlvalidator' }\n" +
                 "yamlValidator {\n" +
-                "\tdirectory = '" + yamlDirectory + "'\n" +
                 "\tallowDuplicates = false\n" +
                 "}", buildFile);
 
-        BuildResult buildResult = GradleRunner
-                .create()
-                .withProjectDir(testProjectDir.getRoot())
-                .withPluginClasspath()
-                .withArguments(TASK_NAME)
-                .buildAndFail();
+        String expectedLineInOutput = yamlFile.getAbsolutePath() + " is not valid.";
 
-        String output = buildResult.getOutput();
-        BuildTask task = buildResult.task(":" + TASK_NAME);
+        expectBuildFailureAndOutput(expectedLineInOutput);
+    }
 
-        assertThat(output, containsString(yamlFile.getAbsolutePath() + " is not valid."));
-        assertThat(task.getOutcome(), is(TaskOutcome.FAILED));
+    @Test
+    public void shouldAllowYamlWithDuplicateKeyWhenDuplicationIsDisabled() {
+
+        writeDuplicateKeyYaml();
+        writeFile("plugins { id 'at.zierler.yamlvalidator' }\n" +
+                "yamlValidator {\n" +
+                "\tallowDuplicates = true\n" +
+                "}", buildFile);
+
+        String expectedLineInOutput = yamlFile.getAbsolutePath() + " is valid.";
+
+        expectBuildSuccessAndOutput(expectedLineInOutput);
     }
 
     @Test
     public void shouldAllowValidYaml() {
 
+        writeDefaultBuildFileWithoutProperties();
         writeFile("framework:\n  key: value\n  other: value\n\nother:\n  other: value\n  key: value", yamlFile);
 
-        BuildResult buildResult = GradleRunner
-                .create()
-                .withProjectDir(testProjectDir.getRoot())
-                .withPluginClasspath()
-                .withArguments(TASK_NAME)
-                .build();
+        String expectedLineInOutput = yamlFile.getAbsolutePath() + " is valid.";
 
-        String output = buildResult.getOutput();
-        BuildTask task = buildResult.task(":" + TASK_NAME);
-
-        assertThat(output, containsString(yamlFile.getAbsolutePath() + " is valid."));
-        assertThat(task.getOutcome(), is(TaskOutcome.SUCCESS));
+        expectBuildSuccessAndOutput(expectedLineInOutput);
     }
 
-    @Test
-    public void shouldNotFailWhenPropertiesAreNotSet() throws IOException {
+    private void writeDefaultBuildFileWithoutProperties() {
 
         writeFile("plugins { id 'at.zierler.yamlvalidator' }", buildFile);
-        String defaultYamlDirectory = ValidationProperties.DEFAULT_DIRECTORY;
-        testProjectDir.newFolder(defaultYamlDirectory.split("/"));
+    }
 
-        BuildResult buildResult = GradleRunner
+    private void writeDuplicateKeyYaml() {
+
+        writeFile("framework:\n  key: value\n\nframework:\n  other: value", yamlFile);
+    }
+
+    private void expectBuildSuccessAndOutput(String expectedLineInOutput) {
+
+        String output = runBuildAndGetOutput();
+
+        assertThat(output, containsString(expectedLineInOutput));
+    }
+
+    private void expectBuildFailureAndOutput(String expectedLineInOutput) {
+
+        String output = runBuildExpectedToFailAndGetOutput();
+
+        assertThat(output, containsString(expectedLineInOutput));
+    }
+
+    private String runBuildAndGetOutput() {
+
+        return createGradleRunner().build().getOutput();
+    }
+
+
+    private String runBuildExpectedToFailAndGetOutput() {
+
+        return createGradleRunner().buildAndFail().getOutput();
+    }
+
+    private GradleRunner createGradleRunner() {
+
+        return GradleRunner
                 .create()
                 .withProjectDir(testProjectDir.getRoot())
                 .withPluginClasspath()
-                .withArguments(TASK_NAME)
-                .build();
-
-        String output = buildResult.getOutput();
-        BuildTask task = buildResult.task(":" + TASK_NAME);
-
-        assertThat(output, containsString("Starting to validate yaml files in " + defaultYamlDirectory + "."));
-        assertThat(task.getOutcome(), is(TaskOutcome.SUCCESS));
+                .withArguments(TASK_NAME);
     }
 
 }
